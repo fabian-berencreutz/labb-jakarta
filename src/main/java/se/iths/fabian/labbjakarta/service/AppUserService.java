7143a3c4f1cdbb8943430a4b395f7ca52bad471e
fabian-berencreutz <fabianberencreutz@gmail.com>
2026-03-11 11:04:30 +0100

    loggning

--- src/main/java/se/iths/fabian/labbjakarta/service/AppUserService.java
+++ src/main/java/se/iths/fabian/labbjakarta/service/AppUserService.java
@@ -7,19 +7,24 @@ import se.iths.fabian.labbjakarta.entity.AppUser;
 import se.iths.fabian.labbjakarta.repository.AppUserRepository;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class AppUserService {
+
+    private static final Logger logger = Logger.getLogger(AppUserService.class.getName());
 
     @Inject
     private AppUserRepository appUserRepository;
 
     @Transactional
     public AppUser saveUser(AppUser user) {
+        logger.info("Försöker spara användare: " + user.getUsername());
         return appUserRepository.save(user);
     }
 
     public List<AppUser> getAllUsers() {
+        logger.info("Hämtar alla användare från databasen.");
         return appUserRepository.findAll();
     }
 }
